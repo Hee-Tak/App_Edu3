003fc49523e5efbe0f3952bac7bd92859f2b91c2
hitak <hitak77@naver.com>
2024-02-24 17:02:04 +0900

    앱 개발 학습 - 네트워크 프로그래밍 개념

---
+++ c85/src/main/java/com/tak/c85/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tak.c85
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+ * 파트23 - 네트워크 프로그래밍으로 서버와 데이터를 주고 받자.
+ *
+ * C84) Concepts of Network Programming
+ *
+ */
